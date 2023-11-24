1a0e8ec0a0cf64eb28508147c145657a6e99efd3
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-11-25 00:35:00 +0500

    updated operations

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -13,9 +13,26 @@ function App() {
     if (value === 'AC') {
       setInputValue('');
       setExpressionParts([]);
+    } else if (value === '%') {
+      // Handle percentage operation
+      handlePercentage();
+    } else if (value === '+/-') {
+      // Handle +/- operation
+      handleToggleSign();
     } else {
       setInputValue((prevValue) => prevValue + value);
       setExpressionParts((prevParts) => [...prevParts, value]);
+    }
+  };
+  const handleToggleSign = () => {
+    // Handle +/- operation if there is a number before "+/-"
+    const lastPartIndex = expressionParts.length - 1;
+    if (lastPartIndex >= 0 && !isNaN(expressionParts[lastPartIndex])) {
+      const lastPart = parseFloat(expressionParts.pop()) * -1;
+      setInputValue(lastPart.toString());
+      setExpressionParts([lastPart.toString()]);
+    } else {
+      setInputValue('Error: Invalid +/-');
     }
   };
 
@@ -26,14 +43,19 @@ function App() {
 
   const handleOperatorClick = (value) => {
     if (value === '=') {
+      // Perform the calculation when '=' is clicked
       calculateResult();
     } else {
+      // Check for consecutive operators
       const lastPart = expressionParts[expressionParts.length - 1];
       if (!isOperator(lastPart)) {
+        // Record the entire expression when an operator is clicked
         setExpressionParts((prevParts) => [...prevParts, value]);
         setInputValue((prevValue) => prevValue + value);
       } else {
+        setExpressionParts([]);
         setInputValue('Error: Consecutive Operators');
+
       }
     }
   };
@@ -42,20 +64,34 @@ function App() {
     try {
       const result = eval(expressionParts.join(''));
       setInputValue(result.toString());
-      setExpressionParts([]);
+      
+      setExpressionParts([result.toString()]); // Set expressionParts to result for further calculations
     } catch (error) {
       setInputValue('Error');
       setExpressionParts([]);
     }
   };
 
+  const handlePercentage = () => {
+    // Handle percentage operation if there is a number before "%"
+    const lastPartIndex = expressionParts.length - 1;
+    if (lastPartIndex >= 0 && !isNaN(expressionParts[lastPartIndex])) {
+      const lastPart = parseFloat(expressionParts.pop()) / 100;
+      setInputValue(lastPart.toString());
+      setExpressionParts([lastPart.toString()]);
+    } else {
+      setInputValue('Error: Invalid Percentage');
+    }
+  };
+
+  // Helper function to check if a string is an operator
   const isOperator = (value) => {
     const operators = ['+', '-', '*', '/'];
     return operators.includes(value);
   };
 
   return (
-    <div className='calc'>
+    <>
       <Input value={inputValue} />
       <div className="obj">
         <div>
@@ -64,7 +100,7 @@ function App() {
         </div>
         <Operators onOperatorClick={handleOperatorClick} />
       </div>
-    </div>
+    </>
   );
 }
 
