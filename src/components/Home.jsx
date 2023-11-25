ffc666811f8ae82cbfba24f8b920b16f31063d51
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2023-11-25 15:54:05 +0500

    Updated Homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,11 @@ import React from 'react'
 const Home = () => {
   return (
     <>
-        <h1>Math Magician</h1>
-        <ul>
-          <li>Home</li>
-          <li>Calculator</li>
-          <li>Quote</li>
-        </ul>
+    <div className='main'>
+      <h1>Welcome to our page</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error perspiciatis nihil non. Assumenda qui alias blanditiis perspiciatis. Tempore officiis hic nulla est! Nulla asperiores quis porro voluptatibus quasi numquam! Excepturi.</p>
+    </div>
     </>
   )
 }
